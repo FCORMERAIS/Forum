bd69bf04f3e288b8b3f0c170444af290f49520a7
Nathan BOURRY <nathan.bourry@ynov.com>
2022-05-20 10:55:20 +0200

    amélioration graphique : créer un post

--- js/Forum.js
+++ js/Forum.js
@@ -1,10 +1,19 @@
 document.getElementsByClassName('triangle')[0]
         .addEventListener('click', function () {
             const popup = document.createElement("div")
+            popup.setAttribute("id","interface_CP");
             popup.classList.add("interface_create_post")
         popup.innerHTML = 
             `
-            <input class="CP_Message" type="text">
+            <p class="CP_Information">Balance ton post :</p>
+            <input class="CP_Message" type="text" placeholder="C'est ici ton blabla ;)">
+            <div class="CP_fermer" id="CP_close")> </div>
+            <p class="ajout_photo">&#128193;</p>
             `
     document.body.appendChild(popup)
-    });
+    document.getElementById('CP_close')
+        .addEventListener('click', async () => {
+            document.getElementById("interface_CP").remove();
+    })
+});
+
