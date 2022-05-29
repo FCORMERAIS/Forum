63b58838f142776be39e52d804b12dce3e414a31
Flavio <flavio.cormerais@ynov.com>
2022-05-29 15:24:42 +0200

    mise en place d'un header interactif en fonction de si on es connécté ou non

--- Go/main.go
+++ Go/main.go
@@ -26,68 +26,85 @@ func GetJson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(GetPostDB())
 }
-
 func Acceuil(w http.ResponseWriter, r *http.Request) {
-	var data User
 	path := r.URL.Path
 	fmt.Println(path)
+	var data User
 	fmt.Println(r.Method)
-	if r.Method == "POST" {
-		var username = r.FormValue("username")
-		var password = r.FormValue("password")
-		var email = r.FormValue("email")
-		var EmailConnect = r.FormValue("email2")
-		var passwordConnect = r.FormValue("password2")
-		if EmailConnect != "" && passwordConnect != "" {
-			fmt.Println("entrer1")
-			passwordAccount := goodMail(EmailConnect)
-			cookie, err := r.Cookie("UserSessionId")
-			if err != nil {
-				cookie = &http.Cookie{
-					Name: "UserSessionId",
+	if path == "/" {
+		path = "../templates/server.html"
+		if r.Method == "POST" {
+			var username = r.FormValue("username")
+			var password = r.FormValue("password")
+			var email = r.FormValue("email")
+			var EmailConnect = r.FormValue("email2")
+			var passwordConnect = r.FormValue("password2")
+			if EmailConnect != "" && passwordConnect != "" {
+				fmt.Println("entrer1")
+				passwordAccount := goodMail(EmailConnect)
+				cookie, err := r.Cookie("UserSessionId")
+				if err != nil {
+					cookie = &http.Cookie{
+						Name: "UserSessionId",
+					}
+				} else {
+					cookie.MaxAge = -1
+					data.Username = "Invité"
+				}
+				if passwordAccount != "" {
+					fmt.Println("entrer2")
+					if CheckPasswordHash(passwordConnect, passwordAccount) {
+						data := connected(EmailConnect)
+						cookie.Value = data.Id
+						cookie.MaxAge = 300
+						http.SetCookie(w, cookie)
+					}
+				} else {
+					fmt.Println("vous n'avez pas rentrer de mot de passe ou le mail n'est pas bon ")
 				}
 			} else {
-				cookie.MaxAge = -1
-			}
-			if passwordAccount != "" {
-				fmt.Println("entrer2")
-				if CheckPasswordHash(passwordConnect, passwordAccount) {
-					data := connected(EmailConnect)
+				if passwordGood(password, w) {
+					passwordHash, err := HashPassword(password)
+					if err != nil {
+						fmt.Println(err)
+					}
+					data := SignUp(email, username, passwordHash)
+					cookie := &http.Cookie{
+						Name: "UserSessionId",
+					}
 					cookie.Value = data.Id
 					cookie.MaxAge = 300
 					http.SetCookie(w, cookie)
+				} else {
+					data.Username = "Invité"
+					fmt.Fprintf(w, "UNE ERREUR EST SURVENUE ")
 				}
-			} else {
-				fmt.Println("vous n'avez pas rentrer de mot de passe ou le mail n'est pas bon ")
-			}
-		} else {
-			if passwordGood(password, w) {
-				passwordHash, err := HashPassword(password)
-				if err != nil {
-					fmt.Println(err)
-				}
-				data := SignUp(email, username, passwordHash)
-				fmt.Println(data)
-				cookie := &http.Cookie{
-					Name: "UserSessionId",
-				}
-				cookie.Value = data.Id
-				cookie.MaxAge = 300
-				http.SetCookie(w, cookie)
-			} else {
-				fmt.Fprintf(w, "UNE ERREUR EST SURVENUE ")
 			}
 		}
+	} else {
+		path = ".." + path
+		cookie, err := r.Cookie("UserSessionId")
+		if err != nil {
+			cookie = &http.Cookie{
+				Name: "UserSessionId",
+			}
+			data.Username = "Invité"
+		} else {
+			data.Username = GetUsernameByID(cookie.Value)
+		}
 	}
-	t, err := template.ParseFiles("../templates/server.html", "../static/style.")
-	if err != nil {
-		fmt.Println("Il y a un problème :", err)
+	if path == "../static/style.css" || path == "../images/Background.png" || path == "../js/index.js" {
+		http.ServeFile(w, r, path)
+	} else {
+		t, err := template.ParseFiles("../templates/server.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		err2 := t.Execute(w, data)
+		if err2 != nil {
+			fmt.Println(err2)
+		}
 	}
-	err2 := t.Execute(w, data)
-	if err2 != nil {
-		fmt.Println("Il y a un problème : ", err2)
-	}
-
 }
 
 func Forum(w http.ResponseWriter, r *http.Request) {
@@ -103,9 +120,13 @@ func Forum(w http.ResponseWriter, r *http.Request) {
 	data := User{
 		Id: cookie.Value,
 	}
-	username := GetUsernameByID(data.Id)
-	data.Username = username
-	t, err := template.ParseFiles("../templates/Forum.html", "../templates/header.html")
+	if data.Id != "Invité" {
+		username := GetUsernameByID(data.Id)
+		data.Username = username
+	} else {
+		data.Username = "Invité"
+	}
+	t, err := template.ParseFiles("../templates/Forum.html")
 	if err != nil {
 		fmt.Printf("error %s \n", err)
 	}
@@ -115,8 +136,5 @@ func Forum(w http.ResponseWriter, r *http.Request) {
 	err2 := t.Execute(w, data)
 	if err2 != nil {
 		fmt.Printf("error2, %s\n", err2)
-	}
-	if r.Method == "POST" {
-		SendPostinDB(r.FormValue("SendPost"))
 	}
 }
