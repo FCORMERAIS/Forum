a707303aa50a9fae8e958e6047bb22ac94be0d4e
Nathan BOURRY <nathan.bourry@ynov.com>
2022-05-30 08:05:04 +0200

    Next post

--- js/Forum.js
+++ js/Forum.js
@@ -8,17 +8,30 @@ document.getElementsByClassName('triangle')[0]
             <p class="CP_Information">Balance ton post :</p>
             <form class="CP_form" method="POST">
                 <input class="CP_Message" name="SendPost" type="text" placeholder="C'est ici ton blabla ;)">
-                <input class="CP_Send" type="submit" value="&#10145">
+                <input class="CP_Send" type="submit" value="&#10145 id="nextPost">
             </form>
             <div class="CP_fermer" id="CP_close"> </div>
             <p class="ajout_photo">&#128193;</p>
             `
     document.body.appendChild(popup)
     document.getElementById('CP_close')
-        .addEventListener('click', async () => {
+        .addEventListener('click', () => {
             document.getElementById("interface_CP").remove();
     })
+    document.getElementById('nextPost')
+        .addEventListener('click', () => {
+            const popupNext = document.createElement("div")
+            popupNext.setAttribute("id","interface_CP");
+            popupNext.classList.add('interface_create_post')
+            popupNext.innerHTML =
+            `
+            <div class="CP_fermer" id="CP_close"> </div>
+
+            `
+        })
 });
+
+
 
 fetch("http://127.0.0.1:5555/donneesJson")
 .then(response => response.json())
